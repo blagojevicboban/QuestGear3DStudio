faa5668d53987617af7fae379a64285c0049af59
Boban Blagojević <boban.blagojevic@tsp.edu.rs>
2026-03-14 21:26:34 +0100

    chore: Remove various development, test, and utility scripts.

--- test_flet.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-
-def main(page: ft.Page):
-    fp = ft.FilePicker()
-    page.overlay.append(fp)
-    page.add(ft.Text("Testing FilePicker..."))
-    page.update()
-
-ft.run(main)
